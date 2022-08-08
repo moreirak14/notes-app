efced6c856647fc87128ce62733dd2fc00d334c4
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-08-08 18:03:08 -0300

    feat: configure dynaconf

---
+++ config.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+from dynaconf import Dynaconf
+
+PATH_ROOT = Path(__file__).parent
+
+settings = Dynaconf(
+    environments=True,
+    envvar_prefix="Notes App",
+    settings_files=['settings.toml', '.secrets.toml'],
+    includes=[f"{PATH_ROOT}/settings.toml", f"{PATH_ROOT}/.secrets.toml"],
+)
