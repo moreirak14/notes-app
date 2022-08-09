1f7a0be96172721a84589a89cd2b9d107e3df20a
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-08-08 22:37:40 -0300

    feat: create router and test with serializers model

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.get_routes, name="routes")
+    path("", views.get_routes, name="routes"),
+    path("notes/", views.get_notes, name="notes"),
+    path("note/<str:pk>", views.get_note, name="note"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
-from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Note
+from .serializers import NoteSerializer
 
-# Create your views here.
 
-
+@api_view(["GET"])
 def get_routes(request):
     routes = [
         {
@@ -38,4 +39,18 @@ def get_routes(request):
         },
     ]
 
-    return JsonResponse(routes, safe=False)
+    return Response(routes)
+
+
+@api_view(["GET"])
+def get_notes(request):
+    notes = Note.objects.all()
+    serializer = NoteSerializer(notes, many=True)
+    return Response(serializer.data)
+
+
+@api_view(["GET"])
+def get_note(request, pk):
+    notes = Note.objects.get(id=pk)
+    serializer = NoteSerializer(notes, many=False)
+    return Response(serializer.data)
